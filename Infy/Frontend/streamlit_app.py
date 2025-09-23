1f4b23946196e710e9bad51c053e4053511322ad
Yeswanthi Sai Bala Sushma Devarakonda <134434556+yeshu909@users.noreply.github.com>
2025-09-23 11:30:52 +0530

    Update streamlit_app.py

--- Infy/Frontend/streamlit_app.py
+++ Infy/Frontend/streamlit_app.py
@@ -21,7 +21,7 @@ try:
     if os.path.exists(model_path):
         model = pickle.load(open(model_path, "rb"))
     else:
-        # st.sidebar.warning("")
+        st.sidebar.warning("")
 except Exception as e:
     st.sidebar.error(f"Error loading model: {e}")
     model = None
@@ -377,5 +377,6 @@ elif selected == "⚙️ Settings":
                            "linear-gradient(135deg, #667eea, #764ba2)", 1300, True)
 
     st.success("✅ Settings applied in real-time!")
+
 
 
